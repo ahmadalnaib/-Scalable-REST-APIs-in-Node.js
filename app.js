83af386cfbc76918fa57ebfd9a03e6048d2d1074
ahmedhabeeb73 <alnaib888@gmail.com>
2024-08-04 18:24:57 +0200

    create controller with route

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const authRouter = require('./route/authRoute');
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// all routes will be here
+app.use('/api/v1/auth', authRouter);
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 }
-);
+)
